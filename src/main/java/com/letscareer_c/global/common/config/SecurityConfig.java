1d4f9178227dc9d6398c3e7c4b982b5e534edcc3
kang yeon joo <rkdduswndi@naver.com>
2024-08-22 20:13:27 +0900

    feat: init security

---
+++ src/main/java/com/letscareer_c/global/common/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.letscareer_c.global.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        //csrf disable
+        http
+                .csrf((auth) -> auth.disable());
+
+        //Form 로그인 방식 disable
+        http
+                .formLogin((auth) -> auth.disable());
+
+        //HTTP Basic 인증 방식 disable
+        http
+                .httpBasic((auth) -> auth.disable());
+
+        return http.build();
+    }
+}
