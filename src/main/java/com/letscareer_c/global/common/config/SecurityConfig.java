15c08582f5f17a7d74c564ff1d18baf66444d157
HoyeongJeon <hoyeong0302@gmail.com>
2024-09-04 22:17:16 +0900

    chore: setting allowed origin for cors

--- src/main/java/com/letscareer_c/global/common/config/SecurityConfig.java
+++ src/main/java/com/letscareer_c/global/common/config/SecurityConfig.java
@@ -1,32 +1,52 @@
 package com.letscareer_c.global.common.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Value("${spring.cors.allowed.origin}")
+    private String allowedOrigin;
+
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
-        //csrf disable
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        // CSRF, form 로그인, HTTP Basic 인증 방식 disable
         http
-                .csrf((auth) -> auth.disable());
-
-        //Form 로그인 방식 disable
-        http
-                .formLogin((auth) -> auth.disable());
-
-        //HTTP Basic 인증 방식 disable
-        http
+                .csrf((auth) -> auth.disable())
+                .formLogin((auth) -> auth.disable())
                 .httpBasic((auth) -> auth.disable());
 
-        http
-                .authorizeHttpRequests((auth) -> auth
-                        .requestMatchers("/**").permitAll());
+        // CORS 설정 추가
+        http.cors((cors) -> cors.configurationSource(corsConfigurationSource()));
+
+        // 모든 요청 허용
+        http.authorizeHttpRequests((auth) -> auth
+                .requestMatchers("/**").permitAll());
 
         return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList(allowedOrigin));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS")); // 허용할 HTTP 메소드 설정
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
