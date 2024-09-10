8f7a35f213411059b703d32183dfd8112a76ec3d
HoyeongJeon <hoyeong0302@gmail.com>
2024-09-11 02:23:51 +0900

    fix: edit RecommendedProgram response

--- src/main/java/com/letscareer_c/domain/program/dao/recommendedProgram/converter/RecommendedProgramConverter.java
+++ src/main/java/com/letscareer_c/domain/program/dao/recommendedProgram/converter/RecommendedProgramConverter.java
@@ -7,6 +7,8 @@ import com.letscareer_c.domain.program.domain.RecommendedProgram;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 import java.util.Optional;
 
 
@@ -25,16 +27,26 @@ public class RecommendedProgramConverter {
         if (program.isEmpty()) {
             throw new IllegalArgumentException("해당 프로그램이 존재하지 않습니다.");
         }
+        long deadline = ChronoUnit.DAYS.between(LocalDateTime.now().toLocalDate(), program.get().getRecruitEndDate().toLocalDate());
+
+        if (deadline == 0L) {
+            deadline = 1L;
+        }
+
 
         return RecommendedProgramDto.builder()
+                .dtype(program.get().getDtype())
                 .recommendedProgramId(recommendedProgram.getRecommendedProgramId())
                 .title(program.get().getTitle())
                 .tag(program.get().getTag())
+                .recruitStatus(program.get().getRecruitStatus())
                 .thumbnail(program.get().getThumbnail())
                 .intro(program.get().getIntro())
+                .recruitStartDate(program.get().getRecruitStartDate().toLocalDate())
                 .recruitEndDate(program.get().getRecruitEndDate().toLocalDate())
                 .programStartDate(program.get().getProgramStartDate().toLocalDate())
                 .programEndDate(program.get().getProgramEndDate().toLocalDate())
+                .deadline(deadline)
                 .build();
     }
 }
